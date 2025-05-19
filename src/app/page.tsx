9803cc2f4022621df91eb771e95b861ed7040431
Chinmay <chinmayjindal108@gmail.com>
2025-05-18 20:25:53 -0400

    fix margin again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen w-full px-4 text-xl sm:text-2xl md:text-3xl">
+      <div className="flex flex-col items-center justify-center min-h-screen w-full px-4 text-xl sm:text-2xl md:text-3xl bg-black">
         <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent min-h-20 text-center">
           SDG Change Challenge 2025
         </p>
-        <p className="sm:mt-10 text-base sm:text-lg md:text-xl lg:text-3xl">
+        <p className="mt-5 sm:mt-10 text-base sm:text-lg md:text-xl lg:text-3xl">
           WIP... Check back later :)
         </p>
       </div>
