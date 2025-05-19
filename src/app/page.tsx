7dc6ae3cda2c27e6e30e3315d285bcbbb0460335
Chinmay <chinmayjindal108@gmail.com>
2025-05-18 21:08:43 -0400

    white text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent min-h-20 text-center">
           SDG Change Challenge 2025
         </p>
-        <p className="mt-5 sm:mt-10 text-base sm:text-lg md:text-xl lg:text-3xl">
+        <p className="mt-5 sm:mt-10 text-base sm:text-lg md:text-xl lg:text-3xl text-white">
           WIP... Check back later :)
         </p>
       </div>
